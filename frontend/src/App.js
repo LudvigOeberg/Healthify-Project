3724c113a83251a77983487dcbf78d1270eef1a7
David Forslöf <davfo018@student.liu.se>
2020-09-17 23:01:17 +0200

    testing CI/CD/K8/docker/react/

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Drawer from './components/Drawer';
+import './assets/App.css';
+
+function App() {
+  return (
+    <div>
+      <Drawer></Drawer>
+    </div>
+  );
+}
+
+export default App;
