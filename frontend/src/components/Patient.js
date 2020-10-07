268db9c01a45ad37bb8fe8a28da3b8b555d3d5df
Ludvig Öberg <ludob894@student.liu.se>
2020-10-07 14:12:29 +0200

    Update to complete task 17.1, 17.2, 17.3. Still needs logic to catch the data

---
+++ frontend/src/components/Patient.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import  { TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import {Button} from '@material-ui/core';
+import InputAdornment from '@material-ui/core/InputAdornment';
+
+
+export default class Patient extends Component {
+    render() {
+        return (
+            <div>
+
+                <h1>Patient vy</h1>
+                <h2> Var vänlig skriv in ditt blodsocker värde</h2>
+                <form className="bloodSugarForm" noValidate autoComplete="off">
+                    <TextField 
+                        required id = "standard-required"
+                        id="standard-basic" 
+                        label="Blodsocker värde" 
+                        variant = "outlined"
+                        InputProps={{
+                                        startAdornment: <InputAdornment position="start">mmol/L</InputAdornment>,
+                                    }}
+                    />
+                </form>
+                <Button 
+                    className="uploadBloodSugarLevel" 
+                    variant="contained" 
+                    color="primary"
+                    onClick={() => { alert('clicked') }}>
+                Skicka in
+                </Button>
+            </div>
+        )
+    }
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    margin: {
+      margin: theme.spacing(1),
+    },
+    withoutLabel: {
+      marginTop: theme.spacing(3),
+    },
+    textField: {
+      width: '25ch',
+    },
+  }));
+  
+  function InputAdornments() {
+    const classes = useStyles();
+    const [values, setValues] = React.useState({
+      amount: '',
+      password: '',
+      weight: '',
+      weightRange: '',
+      showPassword: false,
+    });
+}
