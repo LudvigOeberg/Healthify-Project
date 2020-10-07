cd979a9b67c6395c5051b22e082f0a76d60f9b94
Ludvig Öberg <ludob894@student.liu.se>
2020-10-07 16:02:18 +0200

    fixed state logic and save measurement to localstorage

--- frontend/src/components/Patient.js
+++ frontend/src/components/Patient.js
@@ -1,77 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component, useState} from 'react'
 import  { TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import {Button} from '@material-ui/core';
 import InputAdornment from '@material-ui/core/InputAdornment';
 
-
-
-export default class Patient extends Component {
-    render() {
-        return (
-            <div>
-
-                <h1>Patient vy</h1>
-                <h2> Var vänlig skriv in ditt blodsocker värde</h2>
-                <form className="bloodSugarForm" noValidate autoComplete="off">
-                    <TextField 
-                        required id = "standard-required"
-                        id="blodsugar" 
-                        label="Blodsocker värde" 
-                        variant = "outlined"
-                        InputProps={{
-                                        startAdornment: <InputAdornment position="start">mmol/L</InputAdornment>,
-                                    }}
-                        onChange= {handleTestFieldState(this)}
-                    />
-                </form>
-                <Button 
-                    className="uploadBloodSugarLevel" 
-                    variant="contained" 
-                    color="primary"
-                    onClick={() => { alert('clicked'), localStorage.setItem("BloodSugarValue",) }}>
-                Skicka in
-                </Button>
-            </div>
-        )
+export default function Patient() {
+    var [textValue, setTextValue] = useState('');
+    function setTextState(e) {
+        console.log(e.target.value);
+        setTextValue(e.target.value);
     }
+    
+    return (
+        <div>
+
+            <h1>Patient vy</h1>
+            <h2> Var vänlig skriv in ditt blodsocker värde</h2>
+            <form className="bloodSugarForm" noValidate autoComplete="off">
+                <TextField 
+                    required id = "standard-required"
+                    id="blodsugar" 
+                    label="Blodsocker värde" 
+                    variant = "outlined"
+                    InputProps={{
+                                    startAdornment: <InputAdornment position="start">mmol/L</InputAdornment>,
+                                }}
+                                onChange= {setTextState}
+                />
+            </form>
+            <Button 
+                className="uploadBloodSugarLevel" 
+                variant="contained" 
+                color="primary"
+                onClick={() => { alert('Du registrerade värdet: ' + textValue), localStorage.setItem('BloodSugarValue:' + getCurrentDate(), textValue )}}>
+            Skicka in
+            </Button>
+            <h1> {textValue} </h1>
+        </div>
+    )
 }
 
-function getInitialState() {
-    return {
-        textFieldValue: ''
-    }
-}
-
-function handleTestFieldState(e) {
-    this.setState({
-        textFieldValue: e.target.value
-    });
-}
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-    },
-    margin: {
-      margin: theme.spacing(1),
-    },
-    withoutLabel: {
-      marginTop: theme.spacing(3),
-    },
-    textField: {
-      width: '25ch',
-    },
-  }));
-  
-  function InputAdornments() {
-    const classes = useStyles();
-    const [values, setValues] = React.useState({
-      amount: '',
-      password: '',
-      weight: '',
-      weightRange: '',
-      showPassword: false,
-    });
+export function getCurrentDate(){
+    var today = new Date(); 
+    var todaysDate = String(today.getFullYear()) + '-' +  String(today.getMonth()) + '-' + String(today.getDate());
+    return todaysDate;
 }
