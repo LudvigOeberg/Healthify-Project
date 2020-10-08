a4b2ca95b518f31e7aaa5c39687e99f567df84a0
David Forslöf <davfo018@student.liu.se>
2020-10-09 00:56:28 +0200

    Start of agent ehr api

---
+++ frontend/src/agentEHR.js
@@ -0,0 +1,38 @@
+import superagentPromise from 'superagent-promise';
+import _superagent from 'superagent';
+
+const superagent = superagentPromise(_superagent, global.Promise);
+export const API_ROOT = 'https://rest.ehrscape.com/rest/openehr/v1/';
+const responseBody = res => res.body;
+
+let token = null;
+const tokenPlugin = req => {
+  if (token) {
+    req.set('authorization', `Authorization ${token}`);
+  }
+}
+
+const requests = {
+    del: url => 
+      superagent.del(`${API_ROOT}${url}`).use(tokenPlugin).then(responseBody),
+    get: url => 
+      superagent.get(`${API_ROOT}${url}`).use(tokenPlugin).then(responseBody),
+    put: (url, body) =>
+      superagent.put(`${API_ROOT}${url}`, body).use(tokenPlugin).then(responseBody),
+    post: (url, body) =>
+      superagent.post(`${API_ROOT}${url}`, body).use(tokenPlugin).then(responseBody)
+};
+
+const ehr = {
+    create: () =>
+      requests.post('/ehr'),
+    info: ehr_id =>
+      requests.get('/ehr/' + ehr_id )
+};
+
+
+export default {
+    ehr,
+    setToken: _token => { token = _token; }
+  };
+  
