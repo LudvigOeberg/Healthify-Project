312ab7e3c0f900fd3e5aafe19a49d80401d32470
benim <martin.artan.andersson@gmail.com>
2020-11-07 14:12:25 +0100

    added overview-view into child monitor page

--- frontend/src/components/Child/ChildMonitor.js
+++ frontend/src/components/Child/ChildMonitor.js
@@ -3,6 +3,7 @@ import { withStyles } from '@material-ui/core/styles'
 
 import Container from '@material-ui/core/Container'
 import { connect } from 'react-redux'
+import PersonIcon from '@material-ui/icons/Person'
 import TimeLineChart from '../TimeLineChart'
 
 import {
@@ -55,6 +56,50 @@ class ChildMonitor extends Component {
     return (
       <Container component="main" maxWidth="sm">
         <div className={classes.paper}>
+          <div className={classes.circle}>
+            <div className={classes.avatarCircle}>
+              <PersonIcon className={classes.centerIcon} />
+            </div>
+            <div className={classes.statCircle2}>
+              <div className={classes.smallCircle}>
+                <h1 className={classes.centerText}>50 </h1>
+                <h5 className={classes.centerText}>
+                  <br />g
+                </h5>
+              </div>
+            </div>
+            <div className={classes.statCircle1}>
+              <div className={classes.smallCircle}>
+                <h1 className={classes.centerText}>78 </h1>
+                <h5 className={classes.centerText}>
+                  <br />
+                  Mg/DL
+                </h5>
+              </div>
+            </div>
+            <div className={classes.statCircle3}>
+              <div className={classes.smallCircle}>
+                <h1 className={classes.centerText}>± 1 </h1>
+              </div>
+            </div>
+            <div className={classes.statCircle4}>
+              <div className={classes.smallCircle}>
+                <h1 className={classes.centerText}>78 </h1>
+                <h5 className={classes.centerText}>
+                  <br />
+                  Mg/DL
+                </h5>
+              </div>
+            </div>
+            <div className={classes.statCircle5}>
+              <div className={classes.smallCircle}>
+                <h2 className={classes.centerText}>0 </h2>
+                <h2 className={classes.centerText}>
+                  <br />0
+                </h2>
+              </div>
+            </div>
+          </div>
           <h2>Blodsocker</h2>
           <TimeLineChart
             chartData={bloodsugarData ? Reformat.bloodsugar(bloodsugarData, false, true) : null}
@@ -74,15 +119,85 @@ class ChildMonitor extends Component {
 }
 
 const styles = (theme) => ({
+  circle: {
+    marginTop: '-145vw',
+    width: '200vw',
+    height: '200vw',
+    lineHeight: '500px',
+    borderRadius: '50%',
+    backgroundColor: '#17478c',
+    position: 'relative',
+  },
+  smallCircle: {
+    width: '20vw',
+    height: '20vw',
+    borderRadius: '50%',
+    fontSize: '1rem',
+    lineHeight: '40px',
+    color: theme.palette.text.primary,
+    backgroundColor: '#fff',
+  },
+  statCircle1: {
+    position: 'absolute',
+    bottom: '43vw',
+    left: '55vw',
+  },
+  statCircle2: {
+    position: 'absolute',
+    bottom: '20vw',
+    left: '62vw',
+  },
+  statCircle3: {
+    position: 'absolute',
+    bottom: '4vw',
+    left: '90vw',
+  },
+
+  statCircle4: {
+    position: 'absolute',
+    bottom: '20vw',
+    right: '62vw',
+  },
+
+  statCircle5: {
+    position: 'absolute',
+    bottom: '43vw',
+    right: '55vw',
+  },
   paper: {
     marginTop: theme.spacing(8),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+    // overflowX: 'hidden',
   },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+  avatarCircle: {
+    position: 'absolute',
+    width: '40vw',
+    height: '40vw',
+    bottom: '27vw',
+    left: '80vw',
+    borderRadius: '50%',
+    fontSize: '25px',
+    textAlign: 'center',
+    backgroundColor: theme.palette.text.secondary,
+  },
+  centerText: {
+    position: 'absolute',
+    top: '15%',
+    left: '50%',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+
+  centerIcon: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    fontSize: '15vw',
+    color: theme.palette.text.disabled,
   },
   form: {
     width: '100%', // Fix IE 11 issue.
