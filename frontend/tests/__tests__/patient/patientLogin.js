db43cdebcf268e2b72ab662850982f780261c3be
Yahaya Usman <yahus591@student.liu.se>
2020-11-14 03:33:47 +0100

    handled select for age of child

--- frontend/tests/__tests__/patient/patientLogin.js
+++ frontend/tests/__tests__/patient/patientLogin.js
@@ -1,6 +1,4 @@
 let driver
-const { TextsmsTwoTone } = require('@material-ui/icons')
-const { func } = require('prop-types')
 const webdriver = require('selenium-webdriver')
 // const remoteURL = 'http://tddc88-company-2-2020.kubernetes-public.it.liu.se/'
 const localURL = 'http://localhost:4100/'
@@ -18,10 +16,27 @@ beforeAll(() => {
   driver = new webdriver.Builder().withCapabilities(chromeCapabilities).build()
 })
 
-function Person() {
+afterAll(() => {
+  driver.close()
+})
+
+// generate a random date between two dates
+const randomDate = (start, end) => {
+  const date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
+    .toLocaleDateString()
+    .split('/')
+    .join('')
+  return date
+}
+
+function User(user) {
   const randomInt = Math.floor(Math.random() * Math.floor(1000000))
   this.email = `${randomInt}epost@test.se`
-  this.passw = 'password'
+  this.passw = 'passw'
+  this.age = undefined
+  if (user === 'patient') {
+    this.age = randomDate(new Date(2010, 1, 1), new Date(2020, 8, 31))
+  }
 }
 
 async function getHomePage(url) {
@@ -54,7 +69,7 @@ async function login(driver, userPath, user) {
 async function logout(driver) {
   await driver.findElement(webdriver.By.xpath("//span[text()='Logga ut']")).click()
   await driver.wait(webdriver.until.urlIs(`${localURL}login`))
-  expect(await driver.getCurrentUrl()).toEqual(`${localURL}login`)
+  // expect(await driver.getCurrentUrl()).toEqual(`${localURL}login`)
 }
 
 // eslint-disable-next-line no-shadow
@@ -86,12 +101,19 @@ async function registerPatient(driver, patient) {
   await driver.findElement(webdriver.By.id('email')).sendKeys(patient.email)
   await driver.findElement(webdriver.By.id('password')).sendKeys(patient.passw)
   await driver.findElement(webdriver.By.id('confirmPassword')).sendKeys(patient.passw)
-  await driver.findElement(webdriver.By.id('age')).sendKeys(10)
+  await driver.findElement(webdriver.By.id('dateofbirth')).sendKeys(patient.age)
+  await driver
+    .findElement(
+      webdriver.By.xpath(
+        "//div[@class='MuiInputBase-root MuiOutlinedInput-root Mui-focused Mui-focused MuiInputBase-formControl']/div[@class='MuiSelect-root MuiSelect-select MuiSelect-selectMenu MuiSelect-outlined MuiInputBase-input MuiOutlinedInput-input' and 1]",
+      ),
+    )
+    .sendKeys('Man')
   await driver.findElement(webdriver.By.xpath("//span[text()='Registrera']")).click()
   await driver.wait(webdriver.until.urlIs(`${localURL}parent`))
 }
-const childParent = new Person()
-const patient = new Person()
+const childParent = new User('parent')
+const patient = new User('patient')
 test('Invalid Email address', async () => {
   await getHomePage(localURL)
   await register(driver, childParent)
@@ -99,7 +121,7 @@ test('Invalid Email address', async () => {
   await logout(driver)
   patient.email = 'wrongemail.se'
   await login(driver, 'child', patient).catch(async () => {
-    // const text = await driver.findElement(webdriver.By.id('email-helper-text'), 10000).getText()
-    // await expect(text).toEqual('Not a valid email address')
+    const text = await driver.findElement(webdriver.By.id('email-helper-text'), 10000).getText()
+    expect(text).toEqual('Not a valid email address')
   })
 })
