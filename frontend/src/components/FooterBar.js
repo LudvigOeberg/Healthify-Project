1f2032704254eec0a1b5bbfd75f4f1999f3cd27d
Martin Dagermo <marda785@student.liu.se>
2020-11-17 20:56:43 +0100

    finished footerbar

--- frontend/src/components/FooterBar.js
+++ frontend/src/components/FooterBar.js
@@ -1,19 +1,16 @@
-import Typography from '@material-ui/core/Typography'
-import Link from '@material-ui/core/Link'
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import Container from '@material-ui/core/Container'
-import { Grid, Avatar, Paper } from '@material-ui/core'
+import { Grid, Paper, Tooltip, Fab } from '@material-ui/core'
 import AddIcon from '@material-ui/icons/Add'
 import HomeIcon from '@material-ui/icons/Home';
 import FlashOnIcon from '@material-ui/icons/FlashOn';
 import TimelineIcon from '@material-ui/icons/Timeline';
 import FitnessCenterIcon from '@material-ui/icons/FitnessCenter';
+import { borders } from '@material-ui/system';
 
 const useStyles = makeStyles((theme) => ({
     root: {
     display: 'flex',
-    
     },
 
     footer: {
@@ -26,11 +23,20 @@ const useStyles = makeStyles((theme) => ({
     },
 
     avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    border: '5px solid white',
+    borderRadius: "50%",
+    backgroundColor: "#59B014",
     position: "absolute",
     left: "50%",
+    marginTop: "-40px",
+    marginLeft: "-40px",
     bottom: "23.81%",
+    width: theme.spacing(10),
+    height: theme.spacing(10),
+    },
+
+    icon: {
+        marginLeft: "50%",
     },
 
     paper: {
@@ -39,40 +45,63 @@ const useStyles = makeStyles((theme) => ({
     },
 }))
 
-export default function Copyright() {
+export default function FooterBar() {
   const classes = useStyles()
+  const child = false;
+
   return (
     <footer className={classes.footer}>
     <Grid container className={classes.root} height="100%">
+
         <Grid item xs = {3}>
-        <Paper className={classes.paper} square variant = "outlined">
-            <HomeIcon />
-        </Paper>
+        <a href = {child ? "/patient" : "/parent"}>
+        <Tooltip title="Hem" aria-label="home">
+            <Paper className={classes.paper} square variant = "outlined">
+                <HomeIcon className={classes.icon}/>
+            </Paper>        
+        </Tooltip>
+        </a>
         </Grid>
+
         <Grid item xs = {3}>
-        <Paper className={classes.paper} square variant = "outlined">
-            <FlashOnIcon/>
-        </Paper>     
+         <a> 
+        <Tooltip title="Blixt" aria-label="lightning">
+            <Paper className={classes.paper} square variant = "outlined">
+                <FlashOnIcon className={classes.icon}/>
+            </Paper>   
+        </Tooltip>
+        </a>  
         </Grid>
-        {/* <Grid item xs = {2}>  
-            <Avatar className={classes.avatar}>
-            <AddIcon />
-            </Avatar>
-        </Grid> */}
+
         <Grid item xs = {3}>
-        <Paper className={classes.paper} square variant = "outlined">
-            <TimelineIcon/>
-        </Paper>
+        <a>
+        <Tooltip title="Simulera" aria-label="simulation">
+            <Paper className={classes.paper} square variant = "outlined">
+                <TimelineIcon className={classes.icon}/>
+            </Paper>
+        </Tooltip>
+        </a>
         </Grid>
+
         <Grid item xs = {3}>
-        <Paper className={classes.paper} square variant = "outlined">
-            <FitnessCenterIcon/>
-        </Paper>
+        <a>
+        <Tooltip title="Labb" aria-label="lab">
+            <Paper className={classes.paper} square variant = "outlined">
+                <FitnessCenterIcon className={classes.icon}/>
+            </Paper>
+        </Tooltip>
+        </a>
         </Grid>
+
     </Grid>
-    <Avatar className={classes.avatar} >
-        <AddIcon />
-    </Avatar>
+
+    <a>
+        <Tooltip title="Lägg till" aria-label="add">
+        <Fab color="primary" className={classes.avatar}>
+                <AddIcon fontSize = "large"/>
+        </Fab>
+        </Tooltip>
+    </a>
     </footer>
   )
 }
