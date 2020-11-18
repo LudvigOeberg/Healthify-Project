9e0109cdd708f9f53ccdb3d7014e9f098d4c7b27
David Forslöf <davfo018@student.liu.se>
2020-11-18 18:18:36 +0100

    tried to fix child-monitor page

--- frontend/src/components/Child/ChildMonitor.js
+++ frontend/src/components/Child/ChildMonitor.js
@@ -1,65 +1,58 @@
-import React, { Component } from "react";
-import { withStyles } from "@material-ui/core/styles";
-import { connect } from "react-redux";
-import { Box, Container, Grid, Paper } from "@material-ui/core";
-import Typography from "@material-ui/core/Typography";
-import TimeLineChart from "../TimeLineChart";
-import {
-  PATIENT_PAGE_UNLOADED,
-  FIELD_CHANGE,
-  LOAD_PARTY,
-  LOAD_BLOODSUGAR,
-} from "../../constants/actionTypes";
-import agentEHR from "../../agentEHR";
-import CustomPaginationActionsTable from "../TablePagination";
-import Reformat from "../../reformatEHRData";
+import React, { Component } from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import { connect } from 'react-redux'
+import { Box, Container, Grid, Paper } from '@material-ui/core'
+import Typography from '@material-ui/core/Typography'
+import Skeleton from '@material-ui/lab/Skeleton'
+import TimeLineChart from '../TimeLineChart'
+import { PATIENT_PAGE_UNLOADED, FIELD_CHANGE, LOAD_PARTY, LOAD_BLOODSUGAR } from '../../constants/actionTypes'
+import agentEHR from '../../agentEHR'
+import CustomPaginationActionsTable from '../TablePagination'
+import Reformat from '../../reformatEHRData'
 // import smileChild from '../../Static/smile_child.png'
-import smileChild from "../../Static/big_smile_child.png";
-import Skeleton from "@material-ui/lab/Skeleton";
+// import smileChild from '../../Static/big_smile_child.png'
 
 const mapStateToProps = (state) => ({
   ...state.ehr,
   currentUser: state.common.currentUser,
   bloodsugarValue: state.common.bloodsugar,
-});
+})
 
 const mapDispatchToProps = (dispatch) => ({
   onChangeAuth: (key, value) => dispatch({ type: FIELD_CHANGE, key, value }),
   onUnload: () => dispatch({ type: PATIENT_PAGE_UNLOADED }),
   onLoad: (ehrId) => {
-    dispatch({ type: LOAD_PARTY, payload: agentEHR.EHR.getParty(ehrId) });
+    dispatch({ type: LOAD_PARTY, payload: agentEHR.EHR.getParty(ehrId) })
     dispatch({
       type: LOAD_BLOODSUGAR,
       payload: agentEHR.Query.bloodsugar(ehrId, 0, 20),
-    });
+    })
   },
-});
+})
 
 class ChildMonitor extends Component {
   constructor() {
-    super();
-    this.changeAuth = (ev) =>
-      this.props.onChangeAuth(ev.target.id, ev.target.value);
+    super()
+    this.changeAuth = (ev) => this.props.onChangeAuth(ev.target.id, ev.target.value)
   }
 
   componentDidMount() {
-    this.props.onLoad(this.props.currentUser.ehrid);
+    this.props.onLoad(this.props.currentUser.ehrid)
   }
 
   componentWillUnmount() {
-    this.props.onUnload();
+    this.props.onUnload()
   }
 
   render() {
-    const { classes } = this.props;
-    const bloodsugarData = this.props.bloodsugar;
+    const { classes } = this.props
+    const bloodsugarData = this.props.bloodsugar
     return (
       <Container maxWidth="sm">
         <Paper className={classes.paperTop}>
           <Grid container spacing={2} className={classes.stats}>
             <Grid item xs={3}>
-              <div className={classes.circleNew + " " + classes.circleSm}>
-              </div>
+              <div className={`${classes.circleNew} ${classes.circleSm}`}></div>
               <Box textAlign="center">
                 <Typography className={classes.text} variant="body1">
                   Genomsnitt
@@ -67,37 +60,19 @@ class ChildMonitor extends Component {
               </Box>
             </Grid>
             <Grid item xs={6}>
-              <Skeleton
-                animation="false"
-                variant="circle"
-                className={classes.circleNew}
-              />
+              <Skeleton animation="false" variant="circle" className={classes.circleNew} />
             </Grid>
             <Grid item xs={3}>
-              <Skeleton
-                animation="false"
-                variant="circle"
-                className={classes.circleNew + " " + classes.circleSm}
-              />
+              <Skeleton animation="false" variant="circle" className={`${classes.circleNew} ${classes.circleSm}`} />
               <Box textAlign="center">
                 <Typography className={classes.text} variant="body1">
                   Hög/Låg
                 </Typography>
               </Box>
             </Grid>
-            <Grid
-              justify="space-evenly"
-              alignItems="flex-start"
-              container
-              spacing={2}
-              className={classes.circlesUnder}
-            >
+            <Grid justify="space-evenly" alignItems="flex-start" container spacing={2} className={classes.circlesUnder}>
               <Grid item xs={3}>
-                <Skeleton
-                  animation="false"
-                  variant="circle"
-                  className={classes.circleNew}
-                />
+                <Skeleton animation="false" variant="circle" className={classes.circleNew} />
                 <Box textAlign="center">
                   <Typography className={classes.text} variant="body1">
                     Kolhydrater
@@ -105,11 +80,7 @@ class ChildMonitor extends Component {
                 </Box>
               </Grid>
               <Grid item xs={3}>
-                <Skeleton
-                  animation="false"
-                  variant="circle"
-                  className={classes.circleNew + " " + classes.circleSm}
-                />
+                <Skeleton animation="false" variant="circle" className={`${classes.circleNew} ${classes.circleSm}`} />
                 <Box textAlign="center">
                   <Typography className={classes.text} variant="body1">
                     Avvikelse
@@ -117,11 +88,7 @@ class ChildMonitor extends Component {
                 </Box>
               </Grid>
               <Grid item xs={3}>
-                <Skeleton
-                  animation="false"
-                  variant="circle"
-                  className={classes.circleNew}
-                />
+                <Skeleton animation="false" variant="circle" className={classes.circleNew} />
                 <Box textAlign="center">
                   <Typography className={classes.text} variant="body1">
                     Genomsnitt
@@ -138,13 +105,9 @@ class ChildMonitor extends Component {
               <Typography variant="h5">Mina mätningar</Typography>
               <CustomPaginationActionsTable
                 paginate
-                titles={["Datum", "mmol/L"]}
-                columns={["x", "y"]}
-                rows={
-                  bloodsugarData
-                    ? Reformat.bloodsugar(bloodsugarData, false)
-                    : null
-                }
+                titles={['Datum', 'mmol/L']}
+                columns={['x', 'y']}
+                rows={bloodsugarData ? Reformat.bloodsugar(bloodsugarData, false) : null}
               />
             </Paper>
           </Grid>
@@ -152,78 +115,71 @@ class ChildMonitor extends Component {
             <Paper className={classes.paper}>
               <Typography variant="h5"> Blodsocker</Typography>
               <TimeLineChart
-                chartData={
-                  bloodsugarData
-                    ? Reformat.bloodsugar(bloodsugarData, false, true)
-                    : null
-                }
+                chartData={bloodsugarData ? Reformat.bloodsugar(bloodsugarData, false, true) : null}
                 label="Blodsocker (mmol/L)"
               ></TimeLineChart>
             </Paper>
           </Grid>
         </Grid>
       </Container>
-    );
+    )
   }
 }
 
 const styles = (theme) => ({
   circleNew: {
-    width: "100%",
-    paddingTop: "100%",
-    borderRadius: "50%",
-    background: "white",
+    width: '100%',
+    paddingTop: '100%',
+    borderRadius: '50%',
+    background: 'white',
   },
   circleSm: {
-    marginTop: "50%",
+    marginTop: '50%',
   },
   circlesUnder: {
-    marginTop: "-10%",
+    marginTop: '-10%',
   },
   stats: {
     padding: theme.spacing(2),
   },
   text: {
-    color: "#F2F2F2",
-    fontWeight: "500",
+    color: '#F2F2F2',
+    fontWeight: '500',
   },
 
   centerText: {
     // Centers the numbers/text inside the bubble.
-    position: "absolute",
-    top: "23%",
-    left: "50%",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
+    position: 'absolute',
+    top: '23%',
+    left: '50%',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
   },
 
   centerIcon: {
     // Centers the avatar inside the bubble.
-    position: "absolute",
-    top: "52.5%",
-    left: "49%",
-    transform: "translate(-50%, -50%)",
-    maxWidth: "100%",
-    height: "auto",
+    position: 'absolute',
+    top: '52.5%',
+    left: '49%',
+    transform: 'translate(-50%, -50%)',
+    maxWidth: '100%',
+    height: 'auto',
     color: theme.palette.text.disabled,
   },
   form: {
-    width: "100%", // Fix IE 11 issue.
+    width: '100%', // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   paper: {
     // Defines the papers below avatar
-    height: "100%",
+    height: '100%',
     padding: theme.spacing(2),
   },
   paperTop: {
     marginBottom: theme.spacing(2),
-    borderRadius: "0% 0% 30% 30%",
-    background: "#004894",
+    borderRadius: '0% 0% 30% 30%',
+    background: '#004894',
   },
-});
+})
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(withStyles(styles)(ChildMonitor));
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(ChildMonitor))
