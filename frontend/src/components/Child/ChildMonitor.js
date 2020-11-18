c5310640a31a8fc0c891e36a670c43208f68bfe7
David Forslöf <davfo018@student.liu.se>
2020-11-18 18:17:35 +0100

    tried to fix child-monitor page

--- frontend/src/components/Child/ChildMonitor.js
+++ frontend/src/components/Child/ChildMonitor.js
@@ -1,231 +1,229 @@
-import React, { Component } from 'react'
-import { withStyles } from '@material-ui/core/styles'
-import { connect } from 'react-redux'
-import { Grid, Paper } from '@material-ui/core'
-import Typography from '@material-ui/core/Typography'
-import TimeLineChart from '../TimeLineChart'
-import { PATIENT_PAGE_UNLOADED, FIELD_CHANGE, LOAD_PARTY, LOAD_BLOODSUGAR } from '../../constants/actionTypes'
-import agentEHR from '../../agentEHR'
-import CustomPaginationActionsTable from '../TablePagination'
-import Reformat from '../../reformatEHRData'
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { connect } from "react-redux";
+import { Box, Container, Grid, Paper } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import TimeLineChart from "../TimeLineChart";
+import {
+  PATIENT_PAGE_UNLOADED,
+  FIELD_CHANGE,
+  LOAD_PARTY,
+  LOAD_BLOODSUGAR,
+} from "../../constants/actionTypes";
+import agentEHR from "../../agentEHR";
+import CustomPaginationActionsTable from "../TablePagination";
+import Reformat from "../../reformatEHRData";
 // import smileChild from '../../Static/smile_child.png'
-import smileChild from '../../Static/big_smile_child.png'
+import smileChild from "../../Static/big_smile_child.png";
+import Skeleton from "@material-ui/lab/Skeleton";
 
 const mapStateToProps = (state) => ({
   ...state.ehr,
   currentUser: state.common.currentUser,
   bloodsugarValue: state.common.bloodsugar,
-})
+});
 
 const mapDispatchToProps = (dispatch) => ({
   onChangeAuth: (key, value) => dispatch({ type: FIELD_CHANGE, key, value }),
   onUnload: () => dispatch({ type: PATIENT_PAGE_UNLOADED }),
   onLoad: (ehrId) => {
-    dispatch({ type: LOAD_PARTY, payload: agentEHR.EHR.getParty(ehrId) })
+    dispatch({ type: LOAD_PARTY, payload: agentEHR.EHR.getParty(ehrId) });
     dispatch({
       type: LOAD_BLOODSUGAR,
       payload: agentEHR.Query.bloodsugar(ehrId, 0, 20),
-    })
+    });
   },
-})
+});
 
 class ChildMonitor extends Component {
   constructor() {
-    super()
-    this.changeAuth = (ev) => this.props.onChangeAuth(ev.target.id, ev.target.value)
+    super();
+    this.changeAuth = (ev) =>
+      this.props.onChangeAuth(ev.target.id, ev.target.value);
   }
 
   componentDidMount() {
-    this.props.onLoad(this.props.currentUser.ehrid)
+    this.props.onLoad(this.props.currentUser.ehrid);
   }
 
   componentWillUnmount() {
-    this.props.onUnload()
+    this.props.onUnload();
   }
 
   render() {
-    const { classes } = this.props
-    const bloodsugarData = this.props.bloodsugar
+    const { classes } = this.props;
+    const bloodsugarData = this.props.bloodsugar;
     return (
-      <Grid wholeGrid className={classes.bigDiv} spacing={5} maxWidth="xs" disableGutters>
-        <Grid className={classes.mainGrid}>
-          <Grid backgroundCircle className={classes.circle}>
-            <Grid className={classes.avatarCircle}>
-              <img className={classes.centerIcon} src={smileChild} alt="smile child"></img>
+      <Container maxWidth="sm">
+        <Paper className={classes.paperTop}>
+          <Grid container spacing={2} className={classes.stats}>
+            <Grid item xs={3}>
+              <div className={classes.circleNew + " " + classes.circleSm}>
+              </div>
+              <Box textAlign="center">
+                <Typography className={classes.text} variant="body1">
+                  Genomsnitt
+                </Typography>
+              </Box>
             </Grid>
-            <Grid className={classes.statCircle2}>
-              <Grid className={classes.smallCircle}>
-                <h1 className={classes.centerText}>50 </h1>
-                <h5 className={classes.centerText}>
-                  <br />g
-                </h5>
+            <Grid item xs={6}>
+              <Skeleton
+                animation="false"
+                variant="circle"
+                className={classes.circleNew}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <Skeleton
+                animation="false"
+                variant="circle"
+                className={classes.circleNew + " " + classes.circleSm}
+              />
+              <Box textAlign="center">
+                <Typography className={classes.text} variant="body1">
+                  Hög/Låg
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid
+              justify="space-evenly"
+              alignItems="flex-start"
+              container
+              spacing={2}
+              className={classes.circlesUnder}
+            >
+              <Grid item xs={3}>
+                <Skeleton
+                  animation="false"
+                  variant="circle"
+                  className={classes.circleNew}
+                />
+                <Box textAlign="center">
+                  <Typography className={classes.text} variant="body1">
+                    Kolhydrater
+                  </Typography>
+                </Box>
               </Grid>
-            </Grid>
-            <Grid className={classes.statCircle1}>
-              <Grid className={classes.smallCircle}>
-                <h1 className={classes.centerText}>78 </h1>
-                <h5 className={classes.centerText}>
-                  <br />
-                  Mg/DL
-                </h5>
+              <Grid item xs={3}>
+                <Skeleton
+                  animation="false"
+                  variant="circle"
+                  className={classes.circleNew + " " + classes.circleSm}
+                />
+                <Box textAlign="center">
+                  <Typography className={classes.text} variant="body1">
+                    Avvikelse
+                  </Typography>
+                </Box>
               </Grid>
-            </Grid>
-            <Grid className={classes.statCircle3}>
-              <Grid className={classes.smallCircle}>
-                <h1 className={classes.centerText}>± 1 </h1>
-              </Grid>
-            </Grid>
-            <Grid className={classes.statCircle4}>
-              <Grid className={classes.smallCircle}>
-                <h1 className={classes.centerText}>78 </h1>
-                <h5 className={classes.centerText}>
-                  <br />
-                  Mg/DL
-                </h5>
-              </Grid>
-            </Grid>
-            <Grid className={classes.statCircle5}>
-              <Grid className={classes.smallCircle}>
-                <h2 className={classes.centerText}>0 </h2>
-                <h2 className={classes.centerText}>
-                  <br />0
-                </h2>
+              <Grid item xs={3}>
+                <Skeleton
+                  animation="false"
+                  variant="circle"
+                  className={classes.circleNew}
+                />
+                <Box textAlign="center">
+                  <Typography className={classes.text} variant="body1">
+                    Genomsnitt
+                  </Typography>
+                </Box>
               </Grid>
             </Grid>
           </Grid>
-          <Grid belowAvatar>
-            <Grid item className={classes.bigDiv} xs={12}>
-              <Paper className={classes.paper} elevation={3} variant="outlined">
-                <Typography component="h2" variant="h6">
-                  Mina mätningar
-                </Typography>
-                <CustomPaginationActionsTable
-                  paginate
-                  titles={['Datum', 'mmol/L']}
-                  columns={['x', 'y']}
-                  rows={bloodsugarData ? Reformat.bloodsugar(bloodsugarData, false) : null}
-                />
-              </Paper>
-            </Grid>
-            <Grid item className={classes.bigDiv} xs={12}>
-              <Paper className={classes.paper} elevation={3} variant="outlined">
-                <Typography component="h2" variant="h6">
-                  {' '}
-                  Blodsocker
-                </Typography>
-                <TimeLineChart
-                  chartData={bloodsugarData ? Reformat.bloodsugar(bloodsugarData, false, true) : null}
-                  label="Blodsocker (mmol/L)"
-                ></TimeLineChart>
-              </Paper>
-            </Grid>
+        </Paper>
+
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <Typography variant="h5">Mina mätningar</Typography>
+              <CustomPaginationActionsTable
+                paginate
+                titles={["Datum", "mmol/L"]}
+                columns={["x", "y"]}
+                rows={
+                  bloodsugarData
+                    ? Reformat.bloodsugar(bloodsugarData, false)
+                    : null
+                }
+              />
+            </Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <Typography variant="h5"> Blodsocker</Typography>
+              <TimeLineChart
+                chartData={
+                  bloodsugarData
+                    ? Reformat.bloodsugar(bloodsugarData, false, true)
+                    : null
+                }
+                label="Blodsocker (mmol/L)"
+              ></TimeLineChart>
+            </Paper>
           </Grid>
         </Grid>
-      </Grid>
-    )
+      </Container>
+    );
   }
 }
 
 const styles = (theme) => ({
-  mainGrid: {
-    // Settings for the whole page.
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+  circleNew: {
+    width: "100%",
+    paddingTop: "100%",
+    borderRadius: "50%",
+    background: "white",
+  },
+  circleSm: {
+    marginTop: "50%",
+  },
+  circlesUnder: {
+    marginTop: "-10%",
+  },
+  stats: {
+    padding: theme.spacing(2),
+  },
+  text: {
+    color: "#F2F2F2",
+    fontWeight: "500",
   },
 
-  bigDiv: {
-    // Space between objects
-    overflowX: 'hidden', // Disables sidescroll
-  },
-  circle: {
-    // The big blue background-circle
-    marginTop: '-145vw',
-    width: '200vw',
-    height: '200vw',
-    lineHeight: '500px',
-    borderRadius: '49%',
-    backgroundColor: '#17478c',
-    position: 'relative',
-  },
-  smallCircle: {
-    // The size, color, fontsize for for all statcircles.
-    width: '18vw',
-    height: '18vw',
-    borderRadius: '50%',
-    fontSize: '2.5vw',
-    lineHeight: '8vw',
-    color: theme.palette.text.primary,
-    backgroundColor: '#fff',
-  },
-  statCircle1: {
-    // Statcircle 1-5 determines the position of each bubble.
-    position: 'absolute',
-    bottom: '36vw',
-    left: '57vw',
-  },
-  statCircle2: {
-    position: 'absolute',
-    bottom: '15vw',
-    left: '66vw',
-  },
-  statCircle3: {
-    position: 'absolute',
-    bottom: '4vw',
-    left: '90vw',
-  },
-
-  statCircle4: {
-    position: 'absolute',
-    bottom: '15vw',
-    right: '66vw',
-  },
-
-  statCircle5: {
-    position: 'absolute',
-    bottom: '36vw',
-    right: '57vw',
-  },
-
-  avatarCircle: {
-    // Positions for the bubble around the avatar
-    position: 'absolute',
-    width: '40vw',
-    height: '40vw',
-    bottom: '24vw',
-    left: '80vw',
-    borderRadius: '50%',
-    backgroundColor: '#C4C4C4',
-  },
   centerText: {
     // Centers the numbers/text inside the bubble.
-    position: 'absolute',
-    top: '23%',
-    left: '50%',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
+    position: "absolute",
+    top: "23%",
+    left: "50%",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
   },
 
   centerIcon: {
     // Centers the avatar inside the bubble.
-    position: 'absolute',
-    top: '52.5%',
-    left: '49%',
-    transform: 'translate(-50%, -50%)',
-    maxWidth: '100%',
-    height: 'auto',
+    position: "absolute",
+    top: "52.5%",
+    left: "49%",
+    transform: "translate(-50%, -50%)",
+    maxWidth: "100%",
+    height: "auto",
     color: theme.palette.text.disabled,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   paper: {
     // Defines the papers below avatar
-    height: '100%',
+    height: "100%",
     padding: theme.spacing(2),
   },
-})
+  paperTop: {
+    marginBottom: theme.spacing(2),
+    borderRadius: "0% 0% 30% 30%",
+    background: "#004894",
+  },
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(ChildMonitor))
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(ChildMonitor));
