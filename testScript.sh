569bb076b05454d37c965ec0e8417652748e53cb
David Forslöf <davfo018@student.liu.se>
2020-11-19 15:01:42 +0100

    testing script ci/cd

--- testScript.sh
+++ testScript.sh
@@ -8,11 +8,9 @@ python3 -m venv ./backend/venv
 . ./backend/venv/bin/activate
 pip3 install -r ./backend/requirements/dev.txt
 
-flask db init
-flask db migrate
-flask db upgrade
-flask init-db
+#flask db init
+#flask db migrate
+#flask db upgrade
+#flask init-db
 
-(echo "BACKEND!" && flask run) & (echo "FRONTEND!" && serve -s frontend/build -l 4100) & (sleep 15 && cd frontend && echo "TESTING!" && npm run jest startpage.js)
-
-wait
+flask run & serve -s frontend/build & (sleep 15 && cd frontend && npm run jest)
