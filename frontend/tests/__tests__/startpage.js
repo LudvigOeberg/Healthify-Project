85be10ec7afb4964c33688cf9053c384e8b13459
David Forslöf <davfo018@student.liu.se>
2020-11-21 11:31:12 +0100

    testing in pipeline

--- frontend/tests/__tests__/startpage.js
+++ frontend/tests/__tests__/startpage.js
@@ -1,16 +1,15 @@
 let driver
 const webdriver = require('selenium-webdriver')
+const chrome = require('selenium-webdriver/chrome')
 // const remoteURL = 'http://tddc88-company-2-2020.kubernetes-public.it.liu.se/'
 const localURL = 'http://localhost:4100/'
 
 beforeEach(() => {
-  const chromeCapabilities = webdriver.Capabilities.chrome()
-  // setting chrome options to start the browser fully maximized
-  const chromeOptions = {
-    args: ['--test-type', '--start-maximized'],
-  }
-  chromeCapabilities.set('chromeOptions', chromeOptions)
-  driver = new webdriver.Builder().withCapabilities(chromeCapabilities).build()
+  const options = new chrome.Options()
+  options.addArguments('--test-type')
+  options.addArguments('--start-maximized')
+  options.addArguments('--headless')
+  driver = new webdriver.Builder().forBrowser('chrome').setChromeOptions(options).build()
 })
 
 afterEach(() => {
@@ -22,12 +21,12 @@ async function connectToEHR() {
   await driver.wait(webdriver.until.alertIsPresent())
   const alert1 = await driver.switchTo().alert()
   // replace username with the username for openEHR
-  await alert1.sendKeys('username')
+  await alert1.sendKeys(process.env.ehr_user)
   await alert1.accept()
   await driver.wait(webdriver.until.alertIsPresent())
   const alert2 = await driver.switchTo().alert()
   // replace pass with password from openEHR
-  await alert2.sendKeys('password')
+  await alert2.sendKeys(process.env.ehr_user)
   await alert2.accept()
 }
 
