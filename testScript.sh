82a4ccc8753d34af615706a49b82a5d8a7d113fa
David Forslöf <davfo018@student.liu.se>
2020-11-21 12:06:44 +0100

    testing in pipeline

--- testScript.sh
+++ testScript.sh
@@ -1,19 +1,21 @@
-export FLASK_APP=backend/autoapp.py
+export FLASK_APP=autoapp.py
 export FLASK_ENV=development
 export NODE_ENV=development
 
-python3 -m venv ./backend/venv
-. ./backend/venv/bin/activate
+cd backend
+python3 -m venv venv
+. ./venv/bin/activate
 pip3 install -r ./backend/requirements/dev.txt
 
 flask db init
 flask db migrate
 flask db upgrade
 flask init-db
+cd ..
 
-(echo "BACKEND!" && flask run) & (echo "FRONTEND!" && serve -s frontend/build -l 4100) & (sleep 15 && cd frontend && echo "TESTING!" && npm run jest startpage.js)
+(echo "BACKEND!" && cd backend && flask run) & (echo "FRONTEND!" && serve -s frontend/build -l 4100) & (sleep 15 && cd frontend && echo "TESTING!" && npm run jest startpage.js)
 
-wait %3
+jobs
 kill %1
 kill %2
 echo "Done"
