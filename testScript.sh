4cd68ea132575ece83bfe70e2a02b7c73378e6bd
David Forslöf <davfo018@student.liu.se>
2020-11-22 16:00:28 +0100

    testing in pipeline

--- testScript.sh
+++ testScript.sh
@@ -22,7 +22,9 @@ pip3 install -r ./requirements/dev.txt
 flask db init; flask db migrate; flask db upgrade; flask init-db
 
 cd ..
-(echo "BACKEND!" && cd backend && flask run) & (echo "FRONTEND!" && serve -s frontend/build -l 4100) & (sleep 15 && cd frontend && echo "TESTING!" && npm run jest startpage.js && curl -L localhost:5000/swagger-ui && curl -L localhost:4100) &
+(echo "BACKEND!" && cd backend && flask run) & (echo "FRONTEND!" && serve -s frontend/build -l 4100) & (sleep 15 && cd frontend && echo "TESTING!" && npm run jest startpage.js) &
 wait %3
+curl -L localhost:5000/swagger-ui
+curl -L localhost:4100
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 echo "Done"
