3c4fc6fa4722a060669a5a50af8dd6605c5f4d10
David Forslöf <davfo018@student.liu.se>
2020-11-22 15:34:08 +0100

    testing in pipeline

--- testScript.sh
+++ testScript.sh
@@ -22,9 +22,6 @@ flask db init; flask db migrate; flask db upgrade; flask init-db
 
 cd ..
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
-(echo "BACKEND!" && cd backend && flask run) & (echo "FRONTEND!" && serve -s frontend/build -l 4100) & (sleep 15 && cd frontend && echo "TESTING!" && npm run jest startpage.js) &
-
-curl -L localhost:5000/swagger-ui
-curl -L localhost:4100
+(echo "BACKEND!" && cd backend && flask run) & (echo "FRONTEND!" && serve -s frontend/build -l 4100) & (sleep 15 && cd frontend && echo "TESTING!" && npm run jest startpage.js && curl -L localhost:5000/swagger-ui && curl -L localhost:4100) &
 
 echo "Done"
