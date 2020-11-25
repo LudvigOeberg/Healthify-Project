6872ccd348ed5f7688f8c4c1232ec1b8b9cc0b9a
Magnus Trouve Raa <magtr309@student.liu.se>
2020-11-25 01:30:46 -0800

    fixing linter errors

--- frontend/src/components/Parent/ParentRewardPage.js
+++ frontend/src/components/Parent/ParentRewardPage.js
@@ -1,42 +1,22 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles'
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
 import {
-  Accordion,
-  Typography,
-  FormControl,
-  Select,
-  MenuItem,
-  InputLabel,
   Container,
-  Paper,
   Grid,
   Button,
-  Slider,
-  Input,
-  FormHelperText,
-  InputAdornment,
-  OutlinedInput,
 } from '@material-ui/core'
-import AccordionSummary from '@material-ui/core/AccordionSummary'
-import AccordionDetails from '@material-ui/core/AccordionDetails'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import Card from '@material-ui/core/Card'
 import CardHeader from '@material-ui/core/CardHeader'
 import CardContent from '@material-ui/core/CardContent'
-import InputSlider from '../InputSlider'
-import { Avatar } from '@material-ui/core'
-import EmojiEventsIcon from '@material-ui/icons/EmojiEvents';
-import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
-import Icon from '@material-ui/core/Icon';
+import EmojiEventsIcon from '@material-ui/icons/EmojiEvents'
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn'
 import EditIcon from '@material-ui/icons/Edit'
-import DeleteIcon from '@material-ui/icons/Delete';
+import DeleteIcon from '@material-ui/icons/Delete'
 import { Divider } from '@material-ui/core'
 
 import agentEHR from '../../agentEHR'
