f50c372f77b424a53c81563ce9e2fc71724b695d
Martin Dagermo <marda785@student.liu.se>
2020-11-27 15:23:16 +0100

    New changes parent overview

--- frontend/src/components/Parent/ParentOverview.js
+++ frontend/src/components/Parent/ParentOverview.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
-import { Grid, Paper, ListItemText } from '@material-ui/core'
+import { Grid, Paper, ListItemText, SvgIcon, List, ListItem } from '@material-ui/core'
 import Moment from 'moment'
 import CustomPaginationActionsTable from '../TableOverview'
 import CaregivingTeam from '../CaregivingTeam'
@@ -30,8 +30,6 @@ const mapDispatchToProps = (dispatch) => ({
     else if (disease === 'OBESITY') dispatch({ type: LOAD_WEIGHT, payload: agentEHR.Query.weight(ehrId, limit) })
   },
 })
-
-
 
 const ParentOverview = (props) => {
   const { id } = props.match.params
@@ -82,20 +80,13 @@ const ParentOverview = (props) => {
         <Grid item xs={12} sm={6} md={3}>
           <Grid container spacing={2}>
             <Grid item xs={12}>
-              <Paper className={classes.paper} elevation={2}>
-                <Grid container spacing={2}>
-                  <Grid item xs={3}>
-                    <img src={profileAvatar} alt="Profile" width="100%"></img>
-                  </Grid>
-                  <Grid item xs={9}>
+                <Grid container spacing={2} alignItems = "center" justify = "center" direction="column">
+                    <img src={profileAvatar} alt="Profile" width="30%"></img>
                     <Typography component="h1" variant="h5">
                       {' '}
                       {name}{' '}
                     </Typography>
-                    <ListItemText secondary={disease === 'DIABETES' ? 'Diabetes' : 'Fetma'} />
-                  </Grid>
                 </Grid>
-              </Paper>
 
               <Grid container spacing={1}>
                 <Grid item xs={6} sm={6}>
@@ -122,6 +113,30 @@ const ParentOverview = (props) => {
                     Simulera värden
                   </Button>
                 </Grid>
+              </Grid>
+              <Grid item xs={12}>
+              <Paper className={classes.paper} elevation={2}>
+                <Typography component="h1" variant="h5">
+                    Senaste mätningen
+                </Typography>
+              </Paper>
+              </Grid>
+              <Grid item xs={12}>
+              <Paper className={classes.paper} elevation={2}>
+                  <ListItem disableGutters={true}>
+                  <SvgIcon width="22" height="10" viewBox="2 5 22 12">
+                      <path
+                        d="M19 5h-2V3H7v2H5c-1.1 0-2 .9-2 2v1c0 2.55 1.92 4.63 4.39 4.94.63 1.5 1.98 2.63 3.61 2.96V19H7v2h10v-2h-4v-3.1c1.63-.33 2.98-1.46 3.61-2.96C19.08 12.63 21 10.55 21 8V7c0-1.1-.9-2-2-2zM7 10.82C5.84 10.4 5 9.3 5 8V7h2v3.82zM19 8c0 1.3-.84 2.4-2 2.82V7h2v1z"
+                        fill="primary"
+                        fillOpacity="0.6"
+                    />
+                    </SvgIcon>
+                    {/* <ListItemText primary = "Utmaningar"/> */}
+                    <Typography component="h1" variant="h5" style={{ marginLeft: '.5rem' }}>
+                      {"Utmaningar"}
+                    </Typography>
+                  </ListItem>
+              </Paper>
               </Grid>
               <Grid item xs={12} sm={12}>
                 <Paper className={classes.paper} elevation={2}>
@@ -188,8 +203,12 @@ const styles = makeStyles((theme) => ({
     maxWidth: '100%',
   },
   button: {
-    top: '5px',
-    marginBottom: '5px',
+    top: '20px',
+    marginBottom: '20px',
+  },
+
+  list: {
+    padding: 0,
   },
 
   paper: {
