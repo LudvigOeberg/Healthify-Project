3536f4a56078eb6881a6f66b409c2d39dde28b3b
Sebastian Retzius <sebre055@student.liu.se>
2020-11-27 10:33:39 +0100

    Added a boolean to say wether the inputstepper is a float or int

--- frontend/src/components/InputStepper.js
+++ frontend/src/components/InputStepper.js
@@ -14,7 +14,7 @@ import {  UPDATE_FIELD_AUTH } from '../constants/actionTypes'
  * @param {id} id id of the input
  * @param {input} input the variable that is the input
  * @param {definition} definition a definition of what the input is
- *
+ * @param {int} int boolean wether it need to be int (true) or not (false)
  */
 
 const mapStateToProps = (state) => ({
@@ -27,7 +27,7 @@ const mapDispatchToProps = (dispatch) => ({
   },
 })
 function InputStepper(props) {
-  const { unit, step, min, max, id, input, definition, error } = props
+  const { unit, step, min, max, id, input, definition, error, int } = props
   // const [value, setValue] = React.useState(output);
   // const onChange = (ev) => props.onChange(id, ev.target.value)
   const handleOffset = (val) => props.onChange(id, val)
@@ -70,7 +70,7 @@ function InputStepper(props) {
                 'aria-labelledby': 'input-slider',
               }}
             />
-            <FormHelperText id={`${id}`}>{error ? error : `I heltal mellan ${min} och ${max} `}</FormHelperText>
+            <FormHelperText id={`${id}`}>{error ? error : `I ${int ? 'heltal' : 'decimaltal'} mellan ${min} och ${max} `}</FormHelperText>
           </FormControl>
         </Grid>
       </Grid>
