25d0baf24080d23ba9c27d2d946febccc1158075
Martin Dagermo <marda785@student.liu.se>
2020-12-01 15:42:56 +0100

    New changes to Par overview

--- frontend/src/components/Parent/ParentOverview.js
+++ frontend/src/components/Parent/ParentOverview.js
@@ -3,15 +3,14 @@ import { connect } from 'react-redux'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
-import { Grid, Paper, ListItemText, SvgIcon, List, ListItem } from '@material-ui/core'
-import Moment from 'moment'
-import CustomPaginationActionsTable from '../TableOverview'
+import { Grid, Paper, ListItemText, List, SvgIcon, ListItem } from '@material-ui/core'
 import CaregivingTeam from '../CaregivingTeam'
 import agentEHR from '../../agentEHR'
 import { UPDATE_BOOLEAN, FIELD_CHANGE, LOAD_BLOODSUGAR, LOAD_PARTY, LOAD_WEIGHT } from '../../constants/actionTypes'
-import TimeLineChart from '../TimeLineChart'
-import Reformat from '../../reformatEHRData'
 import profileAvatar from '../../Static/profile_avatar.png'
+import ChildListItemValue from './ChildListItemValue'
+import DeleteIcon from '@material-ui/icons/Delete';
+
 
 const mapStateToProps = (state) => ({
   ...state.common,
@@ -34,32 +33,8 @@ const mapDispatchToProps = (dispatch) => ({
 const ParentOverview = (props) => {
   const { id } = props.match.params
   const disease = props.party ? `${props.party[id].additionalInfo.disease}` : null
-
   const classes = styles()
-  const { bloodsugar } = props
-  const { weight } = props
-  const loading = props.inProgress
   const name = props.party ? `${props.party[id].firstNames} ${props.party[id].lastNames}` : null
-  const input = bloodsugar || weight
-
-  const reformatForChart = (data) => {
-    if (bloodsugar) return Reformat.bloodsugar(data, false, true)
-    if (weight) return Reformat.weight(data, false, true)
-    return null
-  }
-
-  const reformat = (data) => {
-    const dataObjects = []
-    for (let i = 0; i < data.length; i++) {
-      dataObjects.push({
-        time: Moment(data[i].time).format(
-          'YYYY-MM-DD HH:mm',
-        ) /* new Date(data[i].time.substring(0, 16)).toLocaleString() */,
-        value: disease === 'DIABETES' ? `${data[i].value} mmol/L` : `${data[i].weight} kg`,
-      })
-    }
-    return dataObjects
-  }
 
   useEffect(() => {
     props.onLoad(id)
@@ -67,12 +42,25 @@ const ParentOverview = (props) => {
   }, [id, disease]) // eslint-disable-line
 
   const doctor = {
-    name: 'Doktor X',
+    name: 'Doktor Göran',
     org: 'Region Östergötland',
-    mail: 'Dr.x@gmail.com',
+    mail: 'göran@gmail.com',
     telephone: '070-XXX XX XX',
   }
-  const caregivers = [doctor]
+  const psych = {
+    name: 'Psykolog Anders',
+    org: 'Region Östergötland',
+    mail: 'anders@gmail.com',
+    telephone: '070-XXX XX XX',
+  }
+  const nurse = {
+    name: 'Sjuksköterska Karin',
+    org: 'Region Östergötland',
+    mail: 'karre@gmail.com',
+    telephone: '070-XXX XX XX',
+  }
+
+  const caregivers = [doctor, psych, nurse]
 
   return (
     <div className={classes.main}>
@@ -114,85 +102,104 @@ const ParentOverview = (props) => {
       </Grid>
 
       <Grid container className={classes.root} spacing={2} justify="center" height="100%">
-        <Grid item xs={12}>
-        <Paper className={classes.paper} elevation={2}>
-          <Typography component="h1" variant="h5">
-              Senaste mätningen
-          </Typography>
-          <ListItem disableGutters={true}> 
-          <SvgIcon width="22" height="10" viewBox="2 5 22 12">
-                <path
-                  d="M19 5h-2V3H7v2H5c-1.1 0-2 .9-2 2v1c0 2.55 1.92 4.63 4.39 4.94.63 1.5 1.98 2.63 3.61 2.96V19H7v2h10v-2h-4v-3.1c1.63-.33 2.98-1.46 3.61-2.96C19.08 12.63 21 10.55 21 8V7c0-1.1-.9-2-2-2zM7 10.82C5.84 10.4 5 9.3 5 8V7h2v3.82zM19 8c0 1.3-.84 2.4-2 2.82V7h2v1z"
-                  fill="primary"
-                  fillOpacity="0.6"
-              />
-              </SvgIcon>
-              {/* <ListItemText primary = "Utmaningar"/> */}
-              <Typography component="h1" variant="h5" style={{ marginLeft: '.5rem' }}>
-                {"Utmaningar"}
-              </Typography>
-          </ListItem>
-        </Paper>
-        </Grid>
-        <Grid item xs={12}>
-        <Paper className={classes.paper} elevation={2}>
-            <ListItem disableGutters={true}>
-            <SvgIcon width="22" height="10" viewBox="2 5 22 12">
-                <path
-                  d="M19 5h-2V3H7v2H5c-1.1 0-2 .9-2 2v1c0 2.55 1.92 4.63 4.39 4.94.63 1.5 1.98 2.63 3.61 2.96V19H7v2h10v-2h-4v-3.1c1.63-.33 2.98-1.46 3.61-2.96C19.08 12.63 21 10.55 21 8V7c0-1.1-.9-2-2-2zM7 10.82C5.84 10.4 5 9.3 5 8V7h2v3.82zM19 8c0 1.3-.84 2.4-2 2.82V7h2v1z"
-                  fill="primary"
-                  fillOpacity="0.6"
-              />
-              </SvgIcon>
-              {/* <ListItemText primary = "Utmaningar"/> */}
-              <Typography component="h1" variant="h5" style={{ marginLeft: '.5rem' }}>
-                {"Utmaningar"}
-              </Typography>
-            </ListItem>
-        </Paper>
-        </Grid>
-        <Grid item xs={12} sm={12}>
-          <Paper className={classes.paper} elevation={2}>
-            <Grid container spacing={1} alignItems="center" justify="center">
-              <ListItemText
-                primary="Tidigare mätningar"
-                secondary={disease === 'DIABETES' ? 'Blodsocker' : 'Vikt'}
-              />
-              <Grid item xs={12}>
-                <CustomPaginationActionsTable
-                  columns={['time', 'value']}
-                  loading={loading}
-                  rows={input ? reformat(input, false) : null}
-                  paginate={false}
-                />
-              </Grid>
-            </Grid>
-          </Paper>
-        </Grid>
+        <Grid item xs={12} md={4}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper} elevation={2}>
 
-        <Grid item xs={12} md={6}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <Paper className={classes.paper} elevation={2}>
-                <Typography variant="h5">{disease === 'DIABETES' ? 'Blodsocker' : 'Vikt'}</Typography>
-                <ListItemText secondary="Idag" />
-                <p></p>
-                <TimeLineChart
-                  chartData={input ? reformatForChart(input) : null}
-                  label={`${disease === 'DIABETES' ? 'Blodsocker (mmol/L)' : 'Vikt (kg)'}`}
-                  currSettings={disease === 'DIABETES' ? 'day' : 'month'}
-                  hideRadio
-                ></TimeLineChart>
-              </Paper>
-            </Grid>
+              <Grid container spacing={0}>
+                <Grid item xs={11}>
+                  <Typography component="h1" variant="h5">
+                    Senaste mätningen
+                </Typography>
+                  <ChildListItemValue ehrId={id} partyIn={props.party ? props.party[id] : null} />
+                </Grid>
+                <Grid item xs={1} style={{ alignSelf: 'center' }}>
+                  <a href={`/monitor-child/${id}`}>
+                    <SvgIcon width="22" height="10" viewBox="2 5 22 12">
+                      <path
+                        d="M0 15.885L6.79892 9L0 2.115L2.09312 0L11 9L2.09312 18L0 15.885Z"
+                        fill="#4F4F4F"
+                        fillOpacity="1"
+                      />
+                    </SvgIcon>
+                  </a>
+                </Grid>
+              </Grid>
+
+            </Paper>
+          </Grid>
+
+
+          <Grid item xs={12}>
+            <Paper className={classes.paper} elevation={2}>
+              <Grid container spacing={0}>
+                <Grid item xs={11}>
+                  <ListItem disableGutters={true}>
+                    <SvgIcon width="22" height="10" viewBox="2 5 22 12">
+                      <path
+                        d="M19 5h-2V3H7v2H5c-1.1 0-2 .9-2 2v1c0 2.55 1.92 4.63 4.39 4.94.63 1.5 1.98 2.63 3.61 2.96V19H7v2h10v-2h-4v-3.1c1.63-.33 2.98-1.46 3.61-2.96C19.08 12.63 21 10.55 21 8V7c0-1.1-.9-2-2-2zM7 10.82C5.84 10.4 5 9.3 5 8V7h2v3.82zM19 8c0 1.3-.84 2.4-2 2.82V7h2v1z"
+                        fill="#FFD700"
+                        fillOpacity="1"
+                      />
+                    </SvgIcon>
+                    <Typography component="h1" variant="h5" style={{ marginLeft: '1rem' }}>
+                      {"Utmaningar"}
+                    </Typography>
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText primary="2 nya avklarade övning" secondary="1 aktiv" />
+                  </ListItem>
+                </Grid>
+                <Grid item xs={1} style={{ alignSelf: 'center' }}>
+                  <a href={`/parent-reward`}>
+                    <SvgIcon width="22" height="10" viewBox="2 5 22 12">
+                      <path
+                        d="M0 15.885L6.79892 9L0 2.115L2.09312 0L11 9L2.09312 18L0 15.885Z"
+                        fill="#4F4F4F"
+                        fillOpacity="1"
+                      />
+                    </SvgIcon>
+                  </a>
+                </Grid>
+              </Grid>
+            </Paper>
           </Grid>
         </Grid>
-        <Grid item xs={12} md={3}>
+        <Grid item xs={12} md={4}>
           <Paper className={classes.paper} elevation={2}>
             <Typography variant="h5"> Vårdgivare</Typography>
-            {/* Caregivers ska stå här och annan info. Ändra format. */}
             <CaregivingTeam caregivers={caregivers}></CaregivingTeam>
           </Paper>
+        </Grid>
+      </Grid>
+
+      <Grid spacing={1} className={classes.root} justify="center" alignItems="center" direction="column" container>
+        <Grid item xs={8} md={2}>
+          <Button
+            style={{
+              backgroundColor: "#d11a2a",
+
+            }}
+            startIcon={<DeleteIcon />}
+            id="toChildValuesButton"
+            className={classes.button}
+            variant="contained"
+            href={`/edit-child/${id}`}
+            fullWidth
+          >KOPPLA BORT BARNKONTO
+          </Button>
+
+        </Grid>
+        <Grid item xs={8} md={2}>
+          <Button
+            id="toChildValuesButton"
+            className={classes.button}
+            variant="outlined"
+            color="primary"
+            href={`/parent`}
+            fullWidth
+          >TILLBAKA
+          </Button>
         </Grid>
       </Grid>
     </div>
@@ -202,21 +209,16 @@ const ParentOverview = (props) => {
 const styles = makeStyles((theme) => ({
   root: {
     margin: '0px !important',
-    // display: 'horizontal',
     padding: theme.spacing(1),
     maxWidth: '100%',
   },
   button: {
-    top: '20px',
-    marginBottom: '20px',
-  },
-
-  list: {
-    padding: 0,
+    top: '5px',
+    marginBottom: '5px',
+    padding: '10px 5px 10px 5px',
   },
 
   paper: {
-    // height: '100%',
     padding: theme.spacing(2),
     marginTop: theme.spacing(2),
   },
@@ -227,7 +229,7 @@ const styles = makeStyles((theme) => ({
     textAlign: 'center',
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: '100%',
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -239,3 +241,4 @@ const styles = makeStyles((theme) => ({
 }))
 
 export default connect(mapStateToProps, mapDispatchToProps)(ParentOverview)
+
