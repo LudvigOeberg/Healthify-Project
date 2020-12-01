1c946c04eba8c8f402595b704b354177e78fdb56
Ludvig Öberg <ludob894@student.liu.se>
2020-12-01 16:20:52 +0100

    Update testScript to match master

--- testScript.sh
+++ testScript.sh
@@ -8,41 +8,9 @@ python3 -m venv ./backend/venv
 . ./backend/venv/bin/activate
 pip3 install -r ./backend/requirements/dev.txt
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-flask db init
-flask db migrate
-flask db upgrade
-flask init-db
-
-(echo "BACKEND!" && flask run) & (echo "FRONTEND!" && serve -s frontend/build -l 4100) & (sleep 15 && cd frontend && echo "TESTING!" && npm run jest startpage.js)
-
-wait
-=======
-=======
->>>>>>> master
-=======
->>>>>>> a77d64ad31630c8aa8f51d3885ae010e74c36707
-=======
->>>>>>> Test-webdriver-test-case
 #flask db init
 #flask db migrate
 #flask db upgrade
 #flask init-db
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 flask run & serve -s frontend/build & (sleep 15 && cd frontend && npm run jest)
-<<<<<<< HEAD
->>>>>>> testing script ci/cd
-=======
-flask run & serve -s frontend/build & (sleep 15 && cd frontend && npm run jest)
->>>>>>> master
-=======
-flask run & serve -s frontend/build & (sleep 15 && cd frontend && npm run jest)
->>>>>>> a77d64ad31630c8aa8f51d3885ae010e74c36707
-=======
-
->>>>>>> Test-webdriver-test-case
